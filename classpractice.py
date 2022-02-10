85c12d990a0ab122c2e36c211d8a30db2317e094
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2022-02-10 17:03:27 +0600

    Initial commit

--- classpractice.py
+++ classpractice.py
@@ -12,13 +12,41 @@ class Student:
     def math(self):
         return self.age*self.cgpa
 
-class UndergradStudent():
-    pass
-student1 = Student("Mr","Tom",20,10,3.50)
-print(student1.first,student1.last)
-student2 = Student("Mr","Jerry",19,11,3.20)
-print(student1.first,student1.last)
+    def printing(self):
+        print(f'''First Name: {self.first}
+Last Name: {self.last}
+Age: {self.age}
+ID: {self.id}
+CGPA: {self.cgpa}''')
 
-print(student1.emailbuilder())
-print(student2.emailbuilder())
-print(student1.math())
+
+class UniStudent(Student):
+    def __init__(self,first,last,age,id,cgpa,uni):
+        super().__init__(first,last,age,id,cgpa)
+        self.uni = uni
+
+    def view(self):
+        super().printing()
+        print(self.uni)
+
+
+class CollegeStudent(Student):
+    def __init__(self,first,last,age,id,cgpa):
+        super().__init__(first,last,age,id,cgpa)
+
+
+student1 = UniStudent("Mr","Tom",20,5,3.20,"BUET")
+student1.view()
+
+# student1 = CollegeStudent("Mr","Jerry",19,5,3.3)
+# student1.printing()
+
+
+# student1 = Student("Mr","Tom",20,10,3.50)
+# print(student1.first,student1.last)
+# student2 = Student("Mr","Jerry",19,11,3.20)
+# print(student1.first,student1.last)
+#
+# print(student1.emailbuilder())
+# print(student2.emailbuilder())
+# print(student1.math())
